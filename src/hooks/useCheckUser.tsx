9050e504b77970cc438d020b5474b366a7e51d87
njhaus <njhaus@gmail.com>
2023-12-27 13:29:19 -0600

    Update unused checkuser hook

--- src/hooks/useCheckUser.tsx
+++ src/hooks/useCheckUser.tsx
@@ -1,24 +1,24 @@
-import { iListUser } from "../data/listData";
-import { apiPost } from "../services/api_service";
+// import { iListUser } from "../data/listData";
+// import { apiPost } from "../services/api_service";
 
-const useCheckUser = (user: iListUser, setUser,  ) => {
-    const checkUser = () => {
-      apiPost(slug, body).then((res) => {
-      console.log(res);
-      if (res?.message === "success") {
-        console.log(res);
-        setCurrentUser(res.data);
-      } else if (res?.error) {
-        console.log(res);
-        setErr(res.error);
-      } else {
-        setErr("There was an error processing your request");
-      }
-    });
-  };
-  }
+// const useCheckUser = (user: iListUser, setUser,  ) => {
+//     const checkUser = () => {
+//       apiPost(slug, body).then((res) => {
+//       console.log(res);
+//       if (res?.message === "success") {
+//         console.log(res);
+//         setCurrentUser(res.data);
+//       } else if (res?.error) {
+//         console.log(res);
+//         setErr(res.error);
+//       } else {
+//         setErr("There was an error processing your request");
+//       }
+//     });
+//   };
+//   }
   
-    return 
-}
+//     return 
+// }
 
-export default useCheckUser
+// export default useCheckUser
