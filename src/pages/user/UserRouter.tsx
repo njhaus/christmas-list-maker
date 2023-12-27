a6e88b7922c9dfc0f1e18d7eebf2a6699661eb14
njhaus <njhaus@gmail.com>
2023-12-27 16:44:43 -0600

    Remove unused import

--- src/pages/user/UserRouter.tsx
+++ src/pages/user/UserRouter.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-import { Container, Box } from "@mui/material";
+import { Box } from "@mui/material";
 
 import { useParams } from "react-router-dom";
 
